a14727dd1f02f41fbf6f501dd3aa16ee4eebdabc
Khanh Dao <ngockhanhdao@gmail.com>
2012-04-17 19:51:32 -0700

    license info update

--- Scrabble-Stepladder/stepladderBruteForce.py
+++ Scrabble-Stepladder/stepladderBruteForce.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2011  Khanh Dao
+Copyright (C) 2012  Khanh Dao
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
